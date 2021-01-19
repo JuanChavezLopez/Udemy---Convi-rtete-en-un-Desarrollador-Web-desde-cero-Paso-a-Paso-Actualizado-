baffe1b2b2a72cd8da4b7052e928edd815d0c855
Juan Chavez <jchavez666@gmail.com>
2021-01-19 17:26:16 +0200

    Experto en javascript

---
+++ JAVASCRIPT/1- Introduccion/js/js.js
@@ -0,0 +1,6 @@
+document.getElementsByTagName("h2")[0].innerHTML="Esto es una etiqueta agregada desde un archivo javascript";
+
+// TIPOS DE DATOS
+var numero;
+// alert(Number(numero*5));
+alert(numero);
---
+++ JAVASCRIPT/2-variables/js/js.js
@@ -0,0 +1,8 @@
+
+
+
+
+var nombre ="juan";
+var apellido ="Chavez";
+var edad = 25;
+alert(nombre);
