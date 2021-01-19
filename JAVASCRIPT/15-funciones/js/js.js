495cda8ac2d0cccf3058f999074a65775e17a045
Juan Chavez <jchavez666@gmail.com>
2021-01-19 17:55:10 +0200

    Experto en javascript

---
+++ JAVASCRIPT/15-funciones/js/js.js
@@ -0,0 +1,15 @@
+            // var precioUnidad =25, cantidad=75,total;
+            // total = precioUnidad*cantidad;
+            // alert(total);
+
+            // // -------------------------------------------
+            // var balonFutbol, balonBasquet, pelotaBeisball;
+            // balonFutbol = 15;
+            // balonBasquet =7;
+            // pelotaBeisball =9;
+
+            // totalCompra =balonFutbol+balonBasquet+pelotaBeisball;
+            // alert(totalCompra);
+
+            // -----------------------------------
+            // OPERADORES ARITMETICOS
